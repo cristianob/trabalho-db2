e289d79d4d9b64126dfacbda9860c155315731dc
Wesley <51175134+wesleyaag@users.noreply.github.com>
2021-07-23 18:05:18 -0300

    tabs

--- relatorio/src/components/tabs.js
+++ relatorio/src/components/tabs.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import * as RB from "react-bootstrap";
 
 export default class Show extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state = {selected_tab : "none"};
+        this.state = { selected_tab: "none" };
+        this.handleInput = this.handleInput.bind(this);
     }
 
-    show_tab(){
-        console.log("a");
+    handleInput(e) {
+        let value = e.target.value;
+        this.setState({ selected_tab: value });
+        console.log(this.state.selected_tab);
     }
 
     render() {
         return (
             <div className="select">
                 <label>Escolha o tipo de pesquisa: </label>
-                <RB.ButtonGroup size="lg" aria-label="Seleção" onChange = {(event) => this.setState({selected_tab:event.target.value})}>
-                    <RB.Button variant="secondary" value="select-aerodromo">Aeródromo</RB.Button>
-                    <RB.Button variant="secondary" value="select-metar">Metar</RB.Button>
-                    <RB.Button variant="secondary" value="select-taf">Taf</RB.Button>
-                    <RB.Button variant="secondary" value="select-tsc">Tsc</RB.Button>
+                <RB.ButtonGroup size="lg" aria-label="Seleção">
+                    <RB.Button variant="secondary" value="select-aerodromo" onClick={e => this.handleInput(e, "value")} >Aeródromo</RB.Button>
+                    <RB.Button variant="secondary" value="select-metar" onClick={e => this.handleInput(e, "value")}>Metar</RB.Button>
+                    <RB.Button variant="secondary" value="select-taf" onClick={e => this.handleInput(e, "value")}>Taf</RB.Button>
+                    <RB.Button variant="secondary" value="select-tsc" onClick={e => this.handleInput(e, "value")}>Tsc</RB.Button>
                 </RB.ButtonGroup>
             </div>
         );
